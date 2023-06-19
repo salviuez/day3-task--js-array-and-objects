9a26fb017d89c64d28d38dc757f3fb49b8b6d98e
salviuez <salviuez19@gmail.com>
2023-06-19 15:54:10 +0530

    day3-JS-task

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+The task is create an object .
+"to call the objects to get data from the objects".
+1.using "for in" loop to print the details on console.
+2.using "for loop" to get the details on console.
+
+RESUME Task was created on the html page itself:
+"JSON" type:
+Inside the body tag, i have put script tag and wrote the codes.
+
+did my best, kindly evaluate.
+Thanks...
---
+++ script.js
@@ -0,0 +1,62 @@
+var array = [
+    {
+        "id": "1",
+        "name": "Lura Senger",
+        "email": "Xander_Collier@yahoo.com"
+    },
+    {
+        "id": "2",
+        "name": "Wilburn Weber",
+        "email": "Bennett_Kreiger11@yahoo.com"
+    },
+    {
+        "id": "3",
+        "name": "Tyrique Hahn",
+        "email": "Juston.Altenwerth@yahoo.com"
+    },
+    {
+        "id": "4",
+        "name": "Lenny Bailey",
+        "email": "Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+        "id": "5",
+        "name": "Vladimir Keeling",
+        "email": "Louisa_Walsh18@hotmail.com"
+    },
+    {
+        "id": "6",
+        "name": "Kellie Crona",
+        "email": "Chandler_Abernathy@yahoo.com"
+    },
+    {
+        "id": "7",
+        "name": "Carolina White",
+        "email": "Royal50@hotmail.com"
+    },
+    {
+        "id": "8",
+        "name": "Alfredo Conn",
+        "email": "Clarabelle34@hotmail.com"
+    },
+    {
+        "id": "9",
+        "name": "Stan Shanahan",
+        "email": "Lamar.McClure@hotmail.com"
+    },
+    {
+        "id": "10",
+        "name": "Marvin Fay",
+        "email": "Osbaldo58@hotmail.com"
+    },
+]
+
+//1.for in loop:-
+
+for (var i in array) {
+    console.log(array[i]["id"], array[i]["name"], array[i]["email"]);
+}
+//2.for loop:-
+//   for(var i=0; i<=4; i++){
+//     console.log(array[i]["id"], array[i].name, array[i].email);
+//   }
